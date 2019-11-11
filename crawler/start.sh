0a0beb46435c4bac52b6030a2e6c984f9cf58a9a
Alessandro Sebastiani <sebbalex@gmail.com>
2019-11-11 17:22:54 +0100

    First import

---
+++ crawler/start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+bin/crawler updateipa
+#bin/crawler download-whitelist https://onboarding.developers.italia.it/repo-list whitelist/onboarding-reuse.yml
+#bin/crawler crawl whitelist/*.yml
